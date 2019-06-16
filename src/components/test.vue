dbcae115277579132a6a1bfea462d3bb0c6bb0b1
Logan Wood <lwood3499@gmail.com>
2019-06-16 12:09:15 -0500

    hiding env variables

--- src/components/test.vue
+++ src/components/test.vue
@@ -6,46 +6,6 @@
 </template>
 
 <script>
-	// /spaces/{space_id}/environments/{environment_id}/content_types/{content_type_id}?access_token={access_token}
-	let query;
-	const contentfulBaseAPI = process.env.CONTENTFUL_BASE_API;
-	console.log(contentfulBaseAPI);
-	// const space_id = "c87kqyb1kdox";
-	const space_id = process.env.CONTENTFUL_SPACE_ID;
-	// const environment_id = "master";
-	const environment_id = process.env.CONTENTFUL_ENVIRONMENT_ID;
-	const content_type_id = "quizQuestion";
-	// const access_token = "S_P63NpczcCqZu4R-A4Z-zgZo07kqWRObijmm3MmsKg";
-	const access_token = process.env.CONTENTFUL_ACCESS_TOKEN;
-
-	fetch(
-		// `${contentfulBaseAPI}/spaces/c87kqyb1kdox?access_token=S_P63NpczcCqZu4R-A4Z-zgZo07kqWRObijmm3MmsKg`
-		`${contentfulBaseAPI}/spaces/${space_id}/environments/${environment_id}/entries?access_token=${access_token}`
-	)
-		.then(function(response) {
-			return response.json();
-		})
-		.then(res => {
-			console.log(res);
-		});
-	// .then(function(myJson) {
-	// 	console.log(JSON.stringify(myJson));
-	// });
-	// fetch("https://cdn.contentful.com", {
-	// 	method: "POST",
-	// 	headers: {
-	// 		"content-type": "application/json",
-	// 		authorization: "Bearer S_P63NpczcCqZu4R-A4Z-zgZo07kqWRObijmm3MmsKg"
-	// 	},
-	// 	body: JSON.stringify({
-	// 		query
-	// 	})
-	// })
-	// 	.then(res => res.json())
-	// 	.then(response => {
-	// 		console.log(response);
-	// 		const { data } = response;
-	// 	});
 	export default {
 		data() {
 			return {
